5b130be332de02051b62468c0d085ca31a37f674
Theo Odendaal <theo.odendaal@cpdynamics.co.za>
2025-07-23 21:48:02 +0200

    Just pre-emptively creating new files

---
+++ src/binance/account.rs
@@ -0,0 +1,13 @@
+/*
+
+{
+  "id": "605a6d20-6588-4cb9-afa0-b0ab087507ba",
+  "method": "account.status",
+  "params": {
+    "apiKey": "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A",
+    "signature": "83303b4a136ac1371795f465808367242685a9e3a42b22edb4d977d0696eb45c",
+    "timestamp": 1660801839480
+  }
+}
+
+*/
---
+++ src/binance/trading.rs
@@ -0,0 +1,20 @@
+/*
+{
+  "id": "56374a46-3061-486b-a311-99ee972eb648",
+  "method": "order.place",
+  "params": {
+    "symbol": "BTCUSDT",
+    "side": "SELL",
+    "type": "LIMIT",
+    "timeInForce": "GTC",
+    "price": "23416.10000000",
+    "quantity": "0.00847000",
+    "apiKey": "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A",
+    "signature": "15af09e41c36f3cc61378c2fbe2c33719a03dd5eba8d0f9206fbda44de717c88",
+    "timestamp": 1660801715431
+  }
+}
+*/
+
+// https://developers.binance.com/docs/binance-spot-api-docs/websocket-api/trading-requests
+// Place new order
